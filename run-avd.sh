021bbcc0eb7def5ad06a8935a9a3ba2330cd1f1f
Max Popov <max.popov@gmail.com>
2020-04-09 01:50:51 +0300

    run emulator, etc

---
+++ run-avd.sh
@@ -0,0 +1,2 @@
+#sudo /usr/local/android-studio/bin/studio.sh &
+sudo bash -c "/mnt/d/apps/Android/Sdk/emulator/emulator -avd Pixel27 &"
---
+++ run-local.sh
@@ -0,0 +1,4 @@
+# 8, 9 does not work
+#sudo update-alternatives --config java
+
+react-native run-android
